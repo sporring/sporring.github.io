64cb0ac87e6646e91666d4efae5a638aef2a9593
Jon Sporring <sporring@di.ku.dk>
2024-08-22 14:54:57 +0200

    moved matlab repository to public visibility

--- matlab/RULES/src/exponential.m
+++
@@ -1,3 +0,0 @@
-function y = exponential(x,theta,d)
-
-y = exp(-(x-theta)/d)/d;
--- matlab/RULES/src/normal.m
+++
@@ -1,9 +0,0 @@
-function y = normal(x,m,d)
-
-if d == 0
-  y = inf*ones(size(x));
-elseif d == inf
-  y = zeros(size(x));
-else
-  y = exp(-((x-m)/d).^2/2)/(d*sqrt(2*pi));
-end
